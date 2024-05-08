9b0fc3ba24c1ed345cd59b9325a08cc926a25d51
Sarah Proctor <sproctor950@gmail.com>
2024-05-08 15:30:50 -0700

    :test_tube: adds a file with bad formatting as a test for the linting pipeline

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,16 +3,16 @@ import noteLogo from "../assets/note-logo.png"
 
 const Header = () => {
   return (
-    <header>
-      <div className="flex justify-center">
-        <img
-          src={noteLogo}
-          alt="black graphic of a note and a pencil"
-          className="mx-4 my-2 h-7"
-        />
-      </div>
+      <header>
+          <div className="flex justify-center">
+            <img
+              src={noteLogo}
+              alt='black graphic of a note and a pencil'
+              className="mx-4 my-2 h-7"
+            />
+          </div>
 
-      <hr className="mx-4 mb-4 text-lightGray" />
+          <hr className="mx-4 mb-4 text-lightGray" />
     </header>
   )
 }
