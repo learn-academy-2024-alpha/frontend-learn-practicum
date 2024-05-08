fa7badc025e21ecc15a4539337a29442f6c11097
Sarah Proctor <sproctor950@gmail.com>
2024-05-07 20:55:27 -0700

    :construction: basis of dashboard page

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Dashboard = () => {
+  return (
+    <div className="mt-10 h-[calc(100vh-180px)] overflow-hidden">
+      <div>
+        <h3 className="text-center text-xl my-2">Dashboard</h3>
+      </div>
+    </div>
+  )
+}
+
+export default Dashboard
