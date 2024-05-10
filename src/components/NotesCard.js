1a13adf2c119fd4a3bbc82313ae33c9589110a23
MattBarnabo <mattbarnabo@gmail.com>
2024-05-10 15:28:18 -0500

    adds Notes card component that renders the card as a link, a title, creator, and date as content

---
+++ src/components/NotesCard.js
@@ -0,0 +1,30 @@
+import React from "react"
+import { format } from "date-fns"
+import { Link } from "react-router-dom"
+import { useFetchGet } from "../utilities/useFetchGet"
+
+const NotesCard = ({ note }) => {
+  const { title, creator, created_at } = note
+  const formattedDate = format(created_at, "MMMM dd, yyyy")
+
+  const { data } = useFetchGet(`user/${creator}`)
+  const user = data
+  return (
+    <div className=" 2px border">
+      <Link>
+        <div>
+          <h1 className="text-ellipsis text-nowrap font-extrabold">{title}</h1>
+          <div className=" flex justify-between p-1">
+            <h3 className="text-xs opacity-40">
+              {" "}
+              Creator: {user && user.username}
+            </h3>
+            <h3 className="text-xs opacity-40">{formattedDate}</h3>
+          </div>
+        </div>
+      </Link>
+    </div>
+  )
+}
+
+export default NotesCard
