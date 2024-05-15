4dd4b6e3f912ec750e8842e8fbd79239d28a3112
Rashadjaxon <amirjacksonscholar@gmail.com>
2024-05-15 12:07:48 -0700

    📖 added Indicator for current open folder

--- src/components/FolderSideBar.js
+++ src/components/FolderSideBar.js
@@ -1,17 +1,35 @@
-import React from "react"
+import { useState } from "react"
 import FolderIcon from "../assets/empty-folder-icon.png"
+import { Link } from "react-router-dom"
 
-const FolderSideBar = () => {
+const FolderSideBar = ({ notes }) => {
+  const [inactive, setInactive] = useState("Private")
+  const [active, setActive] = useState("Public")
+
+  const handleClickInactive = () => {
+    if (active === "Public") {
+      setActive("Private")
+      setInactive("Public")
+    } else {
+      setActive("Public")
+      setInactive("Private")
+    }
+  }
   return (
-    <div className="ig:flex h-screen">
+    <div className="h-screen w-[35%]">
       <div className="w-full">
         <div className="flex-row justify-center flex w-full bg-neutral pb-2">
           <img className="w-8 p-1" src={FolderIcon} alt="clear folder icon" />
-          <h1 className="text-2xl ">Public</h1>
+          <h1 className="text-2xl ">{active}</h1>
         </div>
         <div className="flex-row justify-center flex w-full pt-2">
-          <img className="w-8 p-1" src={FolderIcon} alt="clear folder icon" />
-          <h1 className="text-2xl ">Private</h1>
+          <button
+            onClick={handleClickInactive}
+            className="flex-row justify-center flex w-full pt-2"
+          >
+            <img className="w-8 p-1" src={FolderIcon} alt="clear folder icon" />
+            <h1 className="w-[65%] text-2xl">{inactive}</h1>
+          </button>
         </div>
       </div>
     </div>
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -7,11 +7,12 @@ import SideBar from "../components/SideBar"
 const Main = () => {
   const { data } = useFetchGet("notes")
   const notes = data
+  console.log(notes)
   return (
     <div className="h-[calc(100vh-100px)] overflow-hidden">
       <div className="flex h-full">
-        <div className="flex w-96 flex-row bg-lightGray">
-          <FolderSideBar />
+        <div className="flex w-96 flex-row justify-between bg-lightGray">
+          <FolderSideBar notes={notes} />
           <SideBar notes={notes} />
         </div>
         <div className="w-[calc(100vw-16rem)]">
