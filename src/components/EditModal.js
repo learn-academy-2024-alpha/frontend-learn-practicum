56a6b15f41bfa6f1d78ce3a422d279abf976fd0a
MattBarnabo <mattbarnabo@gmail.com>
2024-05-17 18:38:01 -0500

    cleans up edit modal

--- src/components/EditModal.js
+++ src/components/EditModal.js
@@ -1,62 +1,59 @@
-import React, { useReducer } from "react"
+import React, { useEffect, useReducer, useState } from "react"
 import { useForm } from "react-hook-form"
-import { Button, Modal, Radio, Label, TextInput } from "flowbite-react"
+import { Button, Modal, Radio, Label, TextInput, Select } from "flowbite-react"
 import Edit from "../assets/edit.png"
 
-const initialState = {
-  openModal: false,
-  title: "",
-  content: "",
-  public: true,
-  errors: { title: false, content: false }
-}
-
-function reducer(state, action) {
-  switch (action.type) {
-    case "TOGGLE_MODAL":
-      return { ...state, openModal: !state.openModal }
-    case "SET_FIELD":
-      return { ...state, [action.field]: action.value }
-    case "SET_ERROR":
-      return {
-        ...state,
-        errors: { ...state.errors, [action.field]: action.value }
-      }
-    default:
-      throw new Error("Unhandled action type: " + action.type)
-  }
-}
-
-const EditModal = ({ updateNote, user, selectedNote }) => {
-  const [state, dispatch] = useReducer(reducer, initialState)
-
-  const preloadedValues = {
-    title: selectedNote?.title,
-    content: selectedNote?.content,
-    public: selectedNote?.public
-  }
-
-  const handleInputChange = (field, value) => {
-    dispatch({ type: "SET_FIELD", field, value })
-    if (state.errors[field]) {
-      dispatch({ type: "SET_ERROR", field, value: false })
+const EditModal = ({ user, selectedNotePublic, selectedNote }) => {
+  const [openModal, setOpenModal] = useState(false)
+  const [isPublic, setIsPublic] = useState()
+  // const [isPrivate, setIsPrivate] = useState()
+  console.log(selectedNote?.public)
+  const handleInputChange = (value) => {
+    if (value) {
+      setIsPublic(true)
+      // setIsPrivate(false)
+    } else {
+      setIsPublic(false)
+      // setIsPrivate(true)
     }
   }
+  const { handleSubmit } = useForm()
+  const id = selectedNote?.id
 
-  const { register, handleSubmit } = useForm({ defaultValues: preloadedValues })
+  const updateNote = async (updatedNote, id) => {
+    try {
+      const patchResponse = await fetch(`http://localhost:3000/notes/${id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(updatedNote)
+      })
+      if (!patchResponse.ok) {
+        throw new Error("Server responded with status: " + patchResponse.status)
+      }
+      await patchResponse.json()
+    } catch (error) {
+      console.error("Error in updateNote:", error)
+      alert("Oops something went wrong: " + error.message)
+    }
+  }
+  console.log(selectedNotePublic)
 
   const onSubmit = (data) => {
-    updateNote({
-      ...data,
-      id: selectedNote.id
-    })
-    dispatch({ type: "TOGGLE_MODAL" })
+    updateNote(
+      {
+        Public: data
+      },
+      id
+    )
+    console.log(data)
+    setOpenModal(false)
   }
-
   return (
     <>
       {selectedNote?.creator === user?.id && (
-        <button onClick={() => dispatch({ type: "TOGGLE_MODAL" })}>
+        <button onClick={() => setOpenModal(true)}>
           <img
             src={Edit}
             alt="pencil icon for editing notes"
@@ -66,8 +63,8 @@ const EditModal = ({ updateNote, user, selectedNote }) => {
       )}
       <Modal
         className="m-auto h-4/5 w-1/2 bg-gray "
-        show={state.openModal}
-        onClose={() => dispatch({ type: "TOGGLE_MODAL" })}
+        show={openModal}
+        onClose={() => setOpenModal(false)}
       >
         <Modal.Header className="m-3 text-xl font-semibold"></Modal.Header>
         <h1 className="mb-3 text-center text-3xl font-semibold">
@@ -80,22 +77,36 @@ const EditModal = ({ updateNote, user, selectedNote }) => {
               <Radio
                 id="public"
                 name="public"
-                value="true"
-                checked={state.public}
-                onChange={() => handleInputChange("public", true)}
-                {...register("public", { required: true })}
+                value={selectedNote?.public === true}
+                // checked={selectedNote?.public}
+                onChange={() => handleInputChange(true)}
+                // {...register("public", { required: true })}
               />
               <Label htmlFor="public">Public</Label>
               <Radio
                 id="private"
                 name="public"
-                value="false"
-                checked={!state.public}
-                onChange={() => handleInputChange("public", false)}
-                {...register("public", { required: true })}
+                value={selectedNote?.public === false}
+                // checked={!selectedNote?.public}
+                onChange={() => handleInputChange(false)}
+                // {...register("public", { required: true })}
               />
               <Label htmlFor="private">Private</Label>
             </div>
+            {/* <div className="flex w-full justify-center pt-1">
+              <Select
+                className="flex w-1/2"
+                value={selectedNote?.public}
+                onChange={}
+              >
+                <option className="text-center" value={true}>
+                  Public
+                </option>
+                <option className="text-center" value={false}>
+                  Private
+                </option>
+              </Select>
+            </div> */}
             <div className="flex justify-center gap-4">
               <Button
                 type="submit"
